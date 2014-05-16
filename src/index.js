ed934a245f1854650435ba3554ce9cc4801c6313
Zongmin Lei <leizongmin@gmail.com>
2014-05-16 12:25:59 +0800

    注册后台菜单 comments

--- src/index.js
+++ src/index.js
@@ -8,4 +8,24 @@ module.exports = function (ns, plugin, debug) {
 
   plugin.load(__dirname);
 
+  var app = ns('app');
+  app.once('start', function () {
+    // register nav link
+    var links = [
+      {
+        name:  'comment_local_admin_list',
+        type:  2,
+        title: 'Comments',
+        url:   '/admin/comment/list'
+      }
+    ];
+    links.forEach(function (item) {
+      app.call('nav.update', item, function (err) {
+        if (err) {
+          console.error((err && err.stack) || err);
+        }
+      });
+    });
+  });
+
 };
